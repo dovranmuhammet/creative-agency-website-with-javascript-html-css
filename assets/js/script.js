1e4513b2f88823e979a52fcc75e0569a70a7e945
Dovran <dovranrustemov@gmail.com>
2022-11-14 12:30:48 -0500

    Added header scroll down

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,3 +36,22 @@ const closeNavbar = function () {
 }
 
 addEventOnElem(navbarLinks, 'click', closeNavbar)
+
+/**
+ * header & back top btn show when scroll down to 100px
+ */
+
+const header = document.querySelector('[data-header]')
+const backTopBtn = document.querySelector('[data-back-top-btn]')
+
+const headerActive = function () {
+  if (window.scrollY > 80) {
+    header.classList.add('active')
+    backTopBtn.classList.add('active')
+  } else {
+    header.classList.remove('active')
+    backTopBtn.classList.remove('active')
+  }
+}
+
+addEventOnElem(window, 'scroll', headerActive)
